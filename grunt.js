508afb88b30035eeb936eb8eb273b2c7f90a9b3a
Peter Ajtai <peter.ajtai@gmail.com>
2012-10-25 15:03:48 -0700

    grunt.js

---
+++ grunt.js
@@ -0,0 +1,55 @@
+/*global module:false */
+// The line above is used for jshint plugins in your IDE
+
+module.exports = function (grunt) {
+    'use strict';
+
+    var project = {
+
+            dirs: {
+                app     : 'application',
+                live    : 'targets/live',
+                temp    : 'temp'
+            },
+
+            files: {
+                scripts     : '/scripts',
+                vendor      : '/scripts/vendor',
+                any         : '/**/*',
+                dot         : {
+                    javascript  : '.js',
+                    html        : '.html'
+                }
+            }
+        },
+
+        gruntConfig = {
+
+            // allow grunt tasks easy access to the project object
+            project: project,
+
+            // Copy things to a temp dir, and only change things in the temp dir
+            cp: {
+                temp: {
+                    src : project.dirs.app,
+                    dest: project.dirs.temp
+                }
+            },
+
+            useref: {
+                html: project.dirs.temp + project.files.any + project.files.dot.html
+            },
+
+            min: {
+                live: ''
+            }
+        };
+
+    grunt.initConfig(gruntConfig);
+
+    grunt.loadNpmTasks('grunt-cp');
+
+    grunt.loadTasks('./tasks/');
+
+    grunt.registerTask('cp useref');
+};
